ca4048523745d1583a8df495198e9de63c948307
Abel <UO284262@uniovi.es>
2025-02-18 11:43:03 +0100

    Guión finalizado

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ import { registerRootComponent } from "expo";
 
 import App from "./App";
 
+import { colors } from "./src/theme/colors";
+import { space } from "./src/theme/spacing";
+import { sizes } from "./src/theme/sizes";
+import { fonts, fontWeights, fontSizes } from "./src/theme/fonts";
+
+export const theme = {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  sizes,
+  space,
+};
+
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
